6c81761d878e9814be9ed3af6ff443d317ccade0
Sydney <sjbarney99@gmail.com>
2019-03-04 18:17:12 -0700

    first commit

---
+++ Toy 3-4-2019.js
@@ -0,0 +1,17 @@
+const instrument = {
+    guitar: {
+        body: "hollow, and black",
+        brand: "Ibanez",
+        strings: "metal tight wrap",
+        widthOfNeck: "average",
+        numberOfStrings: 4
+    },
+    bass: {
+        body: "solid and black",
+        brand: "Fender",
+        strings: "metal tight wrap",
+        widthOfNeck: "wide",
+        numberOfStrings: 6
+    }
+}
+console.log(instrument.bass.body);
